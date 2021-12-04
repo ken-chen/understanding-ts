b4893a2c72136ec0441871a1afd8674ab2c85b04
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-04 20:47:00 +1100

    Tuples

--- app.ts
+++ app.ts
@@ -6,17 +6,29 @@
 //     age: 38
 // };
 
-const person = {
+const person:{
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+
+} = {
     name: 'Ken Chen',
     age: 38,
-    hobbies: ['Sports','Cooking']
+    hobbies: ['Sports','Cooking'],
+    role: [2,'author']
 };
+
+person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [ 0, 'aas', 'assa']
 
 let favoriteActivities: string[];
 
 favoriteActivities = ['Sports'];
 console.log(person.name);
 
-for (const hobby of person.hobbies){
-    console.log(hobby.toUpperCase());
+for (const hobby of person.role){
+    console.log(hobby);
+    // hobby.map() //error 
 }
