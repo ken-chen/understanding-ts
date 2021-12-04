b18881bdd04d1494b6b85676879a7b2e3bbed494
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-04 20:39:27 +1100

    Array types

--- app.ts
+++ app.ts
@@ -8,7 +8,15 @@
 
 const person = {
     name: 'Ken Chen',
-    age: 38
+    age: 38,
+    hobbies: ['Sports','Cooking']
 };
 
+let favoriteActivities: string[];
+
+favoriteActivities = ['Sports'];
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
