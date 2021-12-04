36fee3c462719058dd4deeda4d2ed5bbcf2f9dd4
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-04 11:00:57 +1100

    add type example

--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
-console.log('Your code goes here...!');
-console.log("I love typescript.11..")
+function add(n1: number,n2: number){
+    return n1 +n2;
+}
+
+const number1 =5;
+const numcer2 =2.8;
+
+const result = add(number1,numcer2);
+console.log(result)
