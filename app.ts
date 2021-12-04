3e9c3f34b89b0e667277b51229892499b7898033
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-04 22:36:57 +1100

    union type

--- app.ts
+++ app.ts
@@ -1,52 +1,16 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-//     name: 'Ken Chen',
-//     age: 38
-// };
+function combine(input1: number | string,input2: number | string){
+    let result;
 
-// const person:{
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-
-// } = {
-//     name: 'Ken Chen',
-//     age: 38,
-//     hobbies: ['Sports','Cooking'],
-//     role: [2,'author']
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {ADMIN = 5, READ_ONLY = 100, AUTHOR = "TEXT"};
-
-const person = {
-    name: 'Ken Chen',
-    age: 38,
-    hobbies: ['Sports','Cooking'],
-    role: Role.ADMIN
-};
-
-
-// person.role.push('admin');
-// person.role[1] = 10;
-// person.role = [ 0, 'aas', 'assa']
-
-let favoriteActivities: any[];
-
-favoriteActivities = ['Sports'];
-console.log(person.name);
-
-for (const hobby of person.hobbies){
-    console.log(hobby);
-    // hobby.map() //error 
+    if(typeof input1 === 'number' && typeof input2 === 'number'){
+        result = input1 + input2
+    }else{
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-if( person.role === Role.AUTHOR){
-    console.log("AUTHOR users");
-}
+const combinedAges = combine (30, 26);
+console.log(combinedAges)
+
+const combineNames = combine ('Max','Anna');
+console.log(combineNames)
---
+++ objs-array-enums.ts
@@ -0,0 +1,52 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Ken Chen',
+//     age: 38
+// };
+
+// const person:{
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+
+// } = {
+//     name: 'Ken Chen',
+//     age: 38,
+//     hobbies: ['Sports','Cooking'],
+//     role: [2,'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {ADMIN = 5, READ_ONLY = 100, AUTHOR = "TEXT"};
+
+const person = {
+    name: 'Ken Chen',
+    age: 38,
+    hobbies: ['Sports','Cooking'],
+    role: Role.ADMIN
+};
+
+
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [ 0, 'aas', 'assa']
+
+let favoriteActivities: any[];
+
+favoriteActivities = ['Sports'];
+console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby);
+    // hobby.map() //error 
+}
+
+if( person.role === Role.AUTHOR){
+    console.log("AUTHOR users");
+}
