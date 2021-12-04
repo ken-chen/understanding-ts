1dfbd098a09b5a9260805717742b1e1f5c5d52a9
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-04 11:52:16 +1100

    number, string, boolean

--- app.ts
+++ app.ts
@@ -1,13 +1,19 @@
-function add(n1: number,n2: number){
-    console.log(typeof number1 );
-    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
-        throw new Error('Incorrect input!');
+function add(n1: number,n2: number, showResult: boolean, phrase: string){
+    
+    const result =n1 + n2;
+    if(showResult){
+
+        console.log(phrase + result)
+    }else{
+        return n1 +n2;
     }
-    return n1 +n2;
+   
 }
 
-const number1 ='5';
-const numcer2 =2.8;
+const number1 = 5;
+const numcer2 = 2.8;
+const printResult = true;
+const resultPhrae = 'Result is: ';
 
-const result = add(number1,numcer2);
+const result = add(number1,numcer2,printResult,resultPhrae);
 console.log(result)
