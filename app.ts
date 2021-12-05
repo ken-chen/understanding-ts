028de18e78876dfaa607ec632159960d218c4277
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-05 15:53:52 +1100

    literal Type

--- app.ts
+++ app.ts
@@ -1,16 +1,32 @@
-function combine(input1: number | string,input2: number | string){
-    let result;
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
+  let result;
 
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
-    }else{
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  //   if (resultConversion === 'as-number'){
+  //       return +result;
+  //   }else{
+  //       return result.toString();
+  //   }
+
+  return result;
 }
 
-const combinedAges = combine (30, 26);
-console.log(combinedAges)
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
 
-const combineNames = combine ('Max','Anna');
-console.log(combineNames)
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combineNames = combine("Max", "Anna", "as-text");
+console.log(combineNames);
