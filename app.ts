1e4b946e8bb12292d0db9918075a707ed2fe20dd
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-05 17:30:09 +1100

    functions as Type

--- app.ts
+++ app.ts
@@ -1,18 +1,24 @@
-function add2 (n1:number,n2:number): number{
-    return n1 +n2;
+function add2(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
 function printResult2(num: number): void {
-    console.log('Result: ' + num);
+  console.log("Result: " + num);
 }
-
 
 function printResult3(num: number): undefined {
-    console.log('Result: ' + num);
-    return;
+  console.log("Result: " + num);
+  return;
 }
 
-printResult2(add2(5,12))
-console.log(printResult2(add2(5,12)))
+printResult2(add2(5, 12));
+console.log(printResult2(add2(5, 12)));
 
 let someValue: undefined;
+
+let combineValues: (a:number, b:number) => number;
+combineValues = add2;
+// combineValues = printResult2;
+// combineValues = 5;
+
+console.log(combineValues(8,8));
