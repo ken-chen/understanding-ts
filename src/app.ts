b172c5a47880ddc4026d1e952d2600b3586000a9
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-06 18:31:32 +1100

    private and public

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,41 @@
 class Department {
-    name: string ;
+    public name: string ;
+    private employees: string[]=[];
 
     constructor(n: string){
         this.name = n;
     }
 
+    //private can apply to method
+
     //type safety
     describe(this:Department){
         console.log("Department: " + this.name);
     }
+
+    addEmployee(employee: string){
+        //validation here
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
-console.log(accounting);
-accounting.describe();
+// console.log(accounting);
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+// accounting.employees[2] = 'Anna';
 
-const accountingCopy = {name: 'DUMMY',describe: accounting.describe}
-accountingCopy.describe();
+accounting.name = "NEW NAME";
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+
+
+// const accountingCopy = {name: 'DUMMY',describe: accounting.describe}
+// accountingCopy.describe();
