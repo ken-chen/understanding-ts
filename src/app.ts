cb2dafc6906b6f0350c09a89d4a72c25c594be5f
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-06 15:11:31 +1100

    Arrow Functions

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,6 @@
-const userName2 = 'Ken';
-// userName2 = 'a';
-
-let age =30;
-age =29;
-
-console.log(29);
-var result;
-function add(a:number,b:number){
-     result = a +b;
-    return result;
-
+//this is a function
+const add4 = (a: number , b: number) => {
+    return a +b;
 }
 
-{
-    let a= 100;
-
-}
-add(1,2);
-console.log(result);
+console.log(add4(2,5));
