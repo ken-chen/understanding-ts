d37b652f890aaa6603a73b8c1147afebb51340e5
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-06 22:15:58 +1100

    Abstract classes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-class Department {
+abstract class Department {
 
     static fiscalYear =2020;
     // private readonly id :string;
     // private name: string ;
     protected employees: string[]=[];
 
-    constructor(private readonly id: string, public name: string){
+    constructor(protected readonly id: string, public name: string){
         // this.name = n;
 
         //can not access the static properties in constructor
@@ -19,10 +19,10 @@ class Department {
     //private can apply to method
 
     //type safety
-    describe(this:Department){
-        console.log("Department: " + this.name);
-        console.log(`Department (${this.id}): ${this.name}`)
-    }
+    abstract describe(this:Department):void
+        // console.log("Department: " + this.name);
+        // console.log(`Department (${this.id}): ${this.name}`)
+    
 
     addEmployee(employee: string){
         //validation here
@@ -45,6 +45,10 @@ class ITDepartment extends Department {
 
     constructor(id:string, public admins:string[]){
         super(id,'IT');
+    }
+
+    describe(){
+        console.log("IT Department - ID: " + this.id)
     }
 }
 
@@ -72,6 +76,9 @@ class AccountingDepartment extends Department{
         this.lastReport = reports[0];
     }
 
+    describe(){
+        console.log('Accounting Department -ID: ' + this.id)
+    }
     addReport(text: string){
         this.reports.push(text);
         this.lastReport = text;
@@ -94,7 +101,7 @@ class AccountingDepartment extends Department{
 const accounting = new AccountingDepartment("d2",[]);
 // console.log("***********mostre: " + accounting.mostRecentReport);
 accounting.addReport("Something went wrong...");
-
+accounting.describe();
 //set just access property
 accounting.mostRecentReport = "Year End Report";
 //get just access like normal property
